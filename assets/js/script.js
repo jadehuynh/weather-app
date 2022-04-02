bbd9be422f67eb702fc735509e0c115837e85026
jadehuynh <jadehuynh08@gmail.com>
2022-04-02 18:19:50 -0500

    working on api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,27 @@
 $('#root');
-
-var apiUrl= 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}';
 var apiKey= '44c239f287406de19fc1d0b60b5fc89b'
-//var apiCoordinate="http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}"
+var apiCitySearch= 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + "," + stateCode + "," + countryCode + '&limit=' + limit + '&appid=' + apiKey;
+var stateCode= 'US-CA'
+var cityName= 'west hills'
+var limit= '5'
+console.log(apiCitySearch)
 
-
-
-var lat=""
-var lon=""
-
-var zipCode = $('#searchCityInput').val("");
 var countryCode = "US"
-var convertZip = 'https://api.openweathermap.org/geo/1.0/zip?zip='+ zipCode + ","+ countryCode + '&appid=' + apiKey;
+var zipCode = $('#searchCityInput').val("");
+var convertZip = 'https://api.openweathermap.org/geo/1.0/zip?zip=' + zipCode + ","+ countryCode + '&appid=' + apiKey;
+//select lat and lon values
+    //splice out "lat": "lat":44.2945 and "lon": in "lon":-93.2818
+        //the code for splicing to retrieve numeric values will go into lat and lon variables
 
-//console.log(convertZip);
 
 
 
-
-function test (convertZip) { 
-    var obj = jQuery.parseJSON(convertZip);
-
+function getCoordinates () { 
+    var retrieve = jQuery.parseJSON(convertZip);
+    retrieve.val()
 }
 
-test();
-console.log(obj)
-
-// fetch(apiUrl).then(function (response) {
-//     if (response.ok) {
-//       response.json().then(function (data) {
-//         displayRepos(data.items, language);
-//       });
-//     } else {
-//       alert('Error: ' + response.statusText);
-//     }
-//   });
+var lat='44.2945'
+var lon='-93.2818'
+var apiUrl= 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon='+ lon + '&appid=' + apiKey;
+//function to take the corrdinates from the convert zipcode api to 

1bc7d987db209f646b9b813e8306b51059f2a41f
jadehuynh <jadehuynh08@gmail.com>
2022-04-02 22:07:48 -0500

    added comments to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,73 @@
-$('#root');
 var apiKey= '44c239f287406de19fc1d0b60b5fc89b'
-var apiCitySearch= 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + "," + stateCode + "," + countryCode + '&limit=' + limit + '&appid=' + apiKey;
-var stateCode= 'US-CA'
-var cityName= 'west hills'
-var limit= '5'
-console.log(apiCitySearch)
+//***tried to practice breaking down the api and seeing what values to make into variables to concatenate
+    // var apiCitySearch= 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + "," + stateCode + "," + countryCode + '&limit=' + limit + '&appid=' + apiKey;
 
-var countryCode = "US"
-var zipCode = $('#searchCityInput').val("");
-var convertZip = 'https://api.openweathermap.org/geo/1.0/zip?zip=' + zipCode + ","+ countryCode + '&appid=' + apiKey;
-//select lat and lon values
-    //splice out "lat": "lat":44.2945 and "lon": in "lon":-93.2818
+var searchInput= document.getElementById('searchCityInput')
+var searchButton= document.getElementById('search-btn')
+
+searchButton.addEventListener('click', function(event) {
+    event.preventDefault()
+    getCoordinates(searchInput.value)
+})
+
+//***attempted to try converting zip code into lat and lon values with api provided below
+    // var countryCode = "US"
+    // var zipCode = $('#searchCityInput').val("");
+    // var convertZip = 'https://api.openweathermap.org/geo/1.0/zip?zip=' + zipCode + ","+ countryCode + '&appid=' + apiKey;
+        //select lat and lon values
+         //splice out "lat": "lat":44.2945 and "lon": in "lon":-93.2818
         //the code for splicing to retrieve numeric values will go into lat and lon variables
 
+function forecast (lon, lat) {
+    var getForecastUrl = 'https://api.openweathermap.org/data/2.5/onecall?'
+    var getLat = "lat=" + lat
+    var getLon = "&lon=" + lon
+    var rest = "&exclude=hourly,minutely,current,alert&units=imperial&appid="
 
-
-
-function getCoordinates () { 
-    var retrieve = jQuery.parseJSON(convertZip);
-    retrieve.val()
+    fetch(getForecastUrl + getLat + getLon + rest + apiKey)
+    .then(function(response){
+        response.json()
+        .then(function(data) {
+            console.log(data)
+        })
+    })
 }
 
-var lat='44.2945'
-var lon='-93.2818'
-var apiUrl= 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon='+ lon + '&appid=' + apiKey;
+function getCoordinates (city) { 
+   var apiCitySearch = 'https://api.openweathermap.org/geo/1.0/direct?q='
+   var rest = "&limit=1&appid="
+
+   fetch(apiCitySearch + city + rest + apiKey)
+   .then(function(response){
+       response.json()
+       .then(function(data) {
+           getWeather(data[0].lon,data[0].lat)
+           forecast(data[0].lon,data[0].lat)
+       })
+   })
+
+}
+
+function getWeather(lon, lat) {
+    var getWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?'
+    var getLat = "lat=" + lat
+    var getLon = "&lon=" + lon
+    var rest = "&units=imperial&limit=1&appid="
+
+    fetch(getWeatherUrl + getLat + getLon + rest + apiKey)
+   .then(function(response){
+       response.json()
+       .then(function(data) {
+           console.log(data)
+       })
+   })
+}
+
+var displayData = function (repos, searchTerm) {
+    if (repos.length === 0) {
+      repoContainerEl.textContent = 'No repositories found.';
+      return;
+    }
+}
+//getCoordinates("")
 //function to take the corrdinates from the convert zipcode api to 
